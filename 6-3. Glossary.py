1b171058fbd290ff57d35da82348e449b5823125
saramtahir <saramtahir33@gmail.com>
2025-05-21 00:23:24 +0500

    Initial commit

---
+++ 6-3. Glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    'list': 'a collection of items in a particular order',
+    'dictionary': 'a collection of key-value pairs',
+    'loop': 'a way to repeat actions',
+    'string': 'a series of characters',
+    'variable': 'a name that stores a value'
+}
+
+for word, meaning in glossary.items():
+    print(f"{word.title()}: {meaning}")
